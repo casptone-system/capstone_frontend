aeafe6636c755e7acd31910bd17c05d810c2aada
anthonyjade-lgtm <anthonyjade.s.bayucan@isu.edu.ph>
2026-08-02 00:50:25 +0800

    style

--- src/views/login/LoginPage.vue
+++ src/views/login/LoginPage.vue
@@ -350,7 +350,7 @@ const handleGithubLogin = async () => {
   align-items: center;
   justify-content: center;
   padding: 3rem 2.5rem;
-  min-height: 100vh;
+  max-height: 100vh;
   overflow-y: auto;
   overflow-x: hidden;
 }
